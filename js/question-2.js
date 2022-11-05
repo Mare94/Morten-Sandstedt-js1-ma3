bb1dc7cba7cd9ee56207ec55d3ec31ecf21c80d0
Mare94 <Morten.Sandstedt@stud.noroff.no>
2022-11-05 18:43:09 +0100

    question 2

---
+++ js/question-2.js
@@ -0,0 +1,28 @@
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=INSERTAPIKEYHERE";
+const apiKey = `a5813d7c6e0648f88b2e340b48bfb7b9`;
+const urlGame = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${apiKey}`;
+
+const resultsContainer = document.querySelector(".results");
+
+async function gameFacts(){
+    const response = await fetch(urlGame);
+
+    console.log(response);
+
+    const data = await response.json;
+
+    console.log(data);
+    
+    const info = data.all; 
+
+    resultsContainer.innerHTML = "";
+
+    for(let i = 0; i < info.length; i++){
+        if(i === 8){
+         break;   
+        }
+
+        resultsContainer.innerHTML += `<div class="result">${info[i]}`
+
+    }
+}
